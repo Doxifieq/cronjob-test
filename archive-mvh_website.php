5d0f002e7ee149098cb1be8bc56baba2f247f01c
doxifieq <doxifieq@gmail.com>
2025-02-20 11:25:59 +0100

    [update]

--- archive-mvh_website.php
+++ archive-mvh_website.php
@@ -14,12 +14,30 @@
             <h1 class="title">Status for websites</h1>
 
             <div class="cards">
+                <?php
+                    if (have_posts()) {
+                        while (have_posts()) {
+                            the_post();
+
+
+                            echo '
+                                <div class="card">
+                                    <h3 class="site">' . the_title() . '</h3>
+                                    <p class="url">' . get_field('website') . '</p>
+
+                                    <p><span class="dot green"></span>Status Code: ' . get_post_meta(get_the_ID(), 'status_code', true) . '</p>
+                                </div>
+                            ';
+                        }
+                    }
+                ?>
+<!-- 
                 <div class="card">
                     <h3 class="site">Google</h3>
                     <p class="url">https://www.google.com</p>
 
                     <p><span class="dot green"></span>Status Code: 200</p>
-                </div>
+                </div> -->
             </div>
         </div>
 
