dbbce7aca212a8f56c6b162acbcfb0483036cf8b
doxifieq <doxifieq@gmail.com>
2025-02-20 12:57:59 +0100

    [update]

--- archive-mvh_website.php
+++ archive-mvh_website.php
@@ -28,9 +28,9 @@
                                 <div class="card">
                                     <h3 class="site">' . get_the_title() . '</h3>
                                     <a class="url" href="' . $website_url . '">' . $website_url . '</a>
-                                    <a class="url" href="' . get_the_permalink() . '">View</a>
-
+                                    
                                     <p class="status"><span class="dot ' . mvh_get_status_code_color($status_code) . '"></span>Status Code: ' . $status_code . '</p>
+                                    <a class="url" href="' . get_the_permalink() . '">View</a>
                                 </div>
                             ';
                         }
