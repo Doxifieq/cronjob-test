de38b35f7aee15effbca9e272e1029eadaaa4440
doxifieq <doxifieq@gmail.com>
2025-02-20 10:26:12 +0100

    [fix]

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function mvh_enqueue_styles() {
     wp_enqueue_style('style', 'style.css');
 }
 
-add_action('wp_enqueue_scripts', 'mvh_enqueue_style');
+add_action('wp_enqueue_scripts', 'mvh_enqueue_styles');
 
 add_theme_support('automatic-feed-links');
 add_theme_support('title-tag');
