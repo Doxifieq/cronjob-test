2e7d78688cf38b2f821889f36b8914a5b6bfd55e
doxifieq <doxifieq@gmail.com>
2025-02-20 10:38:39 +0100

    [fix]

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function mvh_enqueue_styles() {
-    wp_enqueue_style('mvh-style', '/wp-content/themes/cronjob-test/style.css');
+    wp_enqueue_style('mvh-style', '/wp-content/themes/cronjob-test/style.css?dev=' . time());
 }
 
 add_action('wp_enqueue_scripts', 'mvh_enqueue_styles');
